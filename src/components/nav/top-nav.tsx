cb5ecc33d17561c0f0a80cbe992e5c9835cbb4b7
Jones Solano <mjsolano12345@gmail.com>
2026-05-01 11:02:48 +0800

    house keeping

--- src/components/nav/top-nav.tsx
+++ src/components/nav/top-nav.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 import Container from "../container";
-import { ThemeToggle } from "../theme-toggle";
 
 export default function TopNav({ title }: { title: string }) {
   return (
     <Container className="flex h-16 items-center justify-between border-b border-border">
       <h1 className="text-2xl font-medium">{title}</h1>
-      <ThemeToggle />
+      <div className="px-6 pt-4">
+        <form action="/api/auth/logout" method="post">
+          <button className="rounded-md border px-3 py-1.5 text-sm" type="submit">
+            Logout
+          </button>
+        </form>
+      </div>
     </Container>
   );
 }
